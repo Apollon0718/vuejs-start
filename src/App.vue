394129294e3222fec30dd7dedf83ba4dcdba439d
andrei <andrei0701@gmail.com>
2018-09-05 08:12:33 -0700

    start

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+ <template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view />
+    <Skills />
+  </div>
+</template>
+
+<script>
+import Skills from './components/Skills.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Skills
+  }
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
+}
+
+nav {
+  padding: 20px 20px 20px 0;
+}
+
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
+}
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+        <h1>hello {{their_name}}, This is an about page </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing edit, seddo eiusmod tempor i</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Skills',
+        data(){
+            return {
+                their_name: this.$route.params.name
+            }
+        }
+    }
+</script>
